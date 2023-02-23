035acd8180327bc702e2457c4b391f591b170a08
Eric Thuaud <eric.thuaud@insee.fr>
2023-02-23 22:40:43 +0100

    fix - rework firstConnect

--- src/ui/components/UserAccount/firstConnect.jsx
+++ src/ui/components/UserAccount/firstConnect.jsx
@@ -13,7 +13,7 @@ import { UserAccountContext } from "../../context/UserAccount";
 import { buttonDictionary } from "i18n";
 
 export const FirstConnectForm = ({ open, close }) => {
-  const { mySurveys } = useContext(UserAccountContext);
+  const { user } = useContext(UserAccountContext);
 
   const handleClose = () => {
     //PUT User with first connect false ou POST contact event
@@ -40,7 +40,7 @@ export const FirstConnectForm = ({ open, close }) => {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            {mySurveys.length
+            {user.mySurveys.length > 0
               ? "Veuillez vérifier et mettre à jour si nécessaire vos informations de contacts. Vous pourrez ensuite répondre à (l’/aux) enquête(s) via l’onglet “Mes Enquêtes”"
               : "Veuillez vérifier et mettre à jour si nécessaire vos informations de contacts. Vous pourrez ensuite répondre à (l’/aux) enquête(s) via l’onglet “Mes Enquêtes”"}
           </DialogContentText>
